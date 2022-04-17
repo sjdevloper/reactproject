46390150da3b574422d808903bc86d61e28c5e84
sjiang20 <sjiang086@gmail.com>
2022-04-17 16:48:43 -0700

    add2cart

---
+++ add-to-cart/src/data.js
@@ -0,0 +1,36 @@
+import img1 from './img/1.png';
+import img2 from './img/2.png';
+import img3 from './img/3.png';
+
+
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title:"Juicy Grapes",
+            desc:'',
+            price:46,
+
+        },
+        {
+            id: 2,
+            img: img2,
+            title:"Juicy Grapes",
+            desc:'',
+            price:46,
+
+        },
+        {
+            id: 3,
+            img: img3,
+            title:"Juicy Grapes",
+            desc:'',
+            price:46,
+
+        }
+    ],
+}
+
+export default data;
